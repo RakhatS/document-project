be85c28dcd5d663546d031cc73cb203f01a6186d
RakhatS <rakhat.saparbaev@gmail.com>
2024-06-07 01:36:20 +0500

    ____

--- DocumentProject.WebClient/src/app/admin-pages/applications-admin-page/applications-admin-page.component.ts
+++ DocumentProject.WebClient/src/app/admin-pages/applications-admin-page/applications-admin-page.component.ts
@@ -4,6 +4,8 @@ import { ToastrService } from 'ngx-toastr';
 import { Application } from 'src/app/_models/application';
 import { AccessTokenService } from 'src/app/_services/accesstoken.service';
 import { ApplicationService } from 'src/app/_services/application.service';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas'; 
 
 @Component({
   selector: 'app-applications-admin-page',
@@ -15,6 +17,9 @@ export class ApplicationsAdminPageComponent implements OnInit {
   loading: boolean = false;
   applications: Application[] = [];
   selectedStatus: string | null = null;
+
+  isApplicationDetailsModalOpened: boolean = false;
+  selectedApplication: Application | undefined;
 
   constructor(private applicationService: ApplicationService,
     private accessTokenService: AccessTokenService,
@@ -58,5 +63,48 @@ export class ApplicationsAdminPageComponent implements OnInit {
       this.toastr.error(error.message);
       this.loading = false;
     })
+  }
+
+
+  openApplicationDetailsModal(application: Application) {
+    this.selectedApplication = application;
+    this.getApplicationDocument(application);
+    this.isApplicationDetailsModalOpened = true;
+  }
+  closeApplicationDetailsModal() {
+    this.isApplicationDetailsModalOpened = false;
+    this.selectedApplication = undefined;
+  }
+
+
+  getApplicationDocument(application: Application) {
+    this.applicationService.getApplicationDocument(application.id!).subscribe(res => {
+      application.document = res.content;
+
+      this.loading = false;
+    }, error => {
+      this.toastr.error(error.message);
+      this.loading = false;
+    })
+  }
+
+  public downloadApplicationDocumentPdf(application: Application) {
+    this.loading = true;
+
+    var data = document.getElementById('application-document');  //Id of the table
+    html2canvas(data!).then(canvas => {
+      // Few necessary setting options  
+      let imgWidth = 208;
+      let pageHeight = 295;
+      let imgHeight = canvas.height * imgWidth / canvas.width;
+      let heightLeft = imgHeight;
+
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jspdf.jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
+      let position = 0;
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.save('Application-' + application.id + '.pdf'); // Generated PDF   
+      this.loading = false;
+    });
   }
 }
